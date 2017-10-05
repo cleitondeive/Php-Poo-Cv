4e482c4b42dac8020f6035b848746a1782d13b88
cleitondeive <cleitondeive@gmail.com>
2017-10-05 11:13:53 -0400

    Inicio do Curso de Poo_Php-07 Com Prof-Guanabara

---
+++ Inicio_Curso_Poo_Php_Cv/index.php
@@ -0,0 +1,13 @@
+
+  <?php
+  //Inicio do Curso de Poo PHP-07 Curso em Video professor Guanabara...
+
+$inicio = "Ola Mundo seja bem vindo ão curso de Poo da Curso em video com o Prof- Guanabara";
+
+echo "$inicio";
+
+
+
+
+
+  ?>
